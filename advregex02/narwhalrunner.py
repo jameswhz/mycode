2e0a72ddc807afc594f7826bf00ac8d6a69ac616
jameswhz <jameswhz@yahoo.ca>
2018-10-17 17:10:53 -0400

    advlab16 17 23 24

---
+++ advregex02/narwhalrunner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import re
+
+androidsDream = "In the sumptuous and enormous hotel room Rick Deckard sat reading the typed carbon sheets on the two androids Roy and Irmgard Baty. In these two cases telescopic snapshots had been included, fuzzy 3-D color prints which he could barely make out. The woman, he decided, looks attractive. Roy Baty, however, is something different. Something worse. A pharmacist on Mars, he read. Or at least the android had made use of that cover. In actuality it had probably been a manual laborer, a field hand, with aspirations for something better. Do androids dream? Rick asked himself. Evidently; that's why they occasionally kill their employers and flee here. A better life, without servitude. Like Luba Luft; singing Don Giovanni and Le Nozze instead of toiling across the face of a barren rock-strewn field. On a fundamentally uninhabitable colony world."
+
+meetsteven = re.sub(r'Roy', 'Steven', androidsDream)
+
+noandroids = re.sub(r'android', 'narwhal', meetsteven)
+
+print("Our new story featuring Steven Baty and his narwhal friends:\n", noandroids)
